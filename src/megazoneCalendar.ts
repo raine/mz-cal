14bca77198a1f45c7ab0e098cb8932d20a212560
Raine Virta <raine.virta@gmail.com>
2020-01-01 11:04:50 +0200

    delete fully booked events from calendar

--- src/megazoneCalendar.ts
+++ src/megazoneCalendar.ts
@@ -54,6 +54,29 @@ export const updateMzLocationCalendar = async (location: MzLocation) => {
   await pMapSeries(mzEvents, (event) =>
     upsertEvent(calendarId, calendarEvents, event)
   )
+
+  // Because of the current technical limitation that mzEvents only contains
+  // rounds that are not fully booked, we have to determine fully booked rounds
+  // from calendar by comparing all future calendar event timestamps to
+  // timestamps in mzEvents. If also booked events were parsed from the
+  // calender html, this would be a bit easier.
+  const now = DateTime.local()
+  const calendarEventsToBeDeleted = calendarEvents.filter((event) => {
+    const startDateTime =
+      event.start?.dateTime && DateTime.fromISO(event.start.dateTime)
+    return startDateTime && startDateTime >= now
+      ? !mzEvents.some((e) => DateTime.fromISO(e.start).equals(startDateTime))
+      : false
+  })
+
+  await pMapSeries(calendarEventsToBeDeleted, async (event) =>
+    calendar.events
+      .delete({ calendarId, eventId: event.id!, ...(await authenticate()) })
+      .then(() => {
+        console.log('deleted event', stringify({ eventId: event.id! }))
+      })
+  )
+
   // TODO: check future calendar event dates that don't exist in mzEvents and delete them
   console.log('finished updating mz calendar', stringify({ location }))
 }
